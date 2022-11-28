787f9bf8beded5e95a1430b646046df5fde4676e
Ultrapyre <sidneymakhiutao@yahoo.com.hk>
2022-11-28 18:40:57 +0800

    Added links to HTML files within server.js

--- server.js
+++ server.js
@@ -2,5 +2,19 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+//Temporary port till I learn how to use Hiroku
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+
+app.get('/notes', (req, res) =>{
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
+
+app.listen(PORT, () =>
+  console.log(`App open at http://localhost:${PORT}`)
+);
